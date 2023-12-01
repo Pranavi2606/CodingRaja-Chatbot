4c001d0402d35a91274cf599563699ba78f42083
Pranavi2606 <94110080+Pranavi2606@users.noreply.github.com>
2023-12-01 13:20:21 +0530

    Add files via upload

---
+++ Foxy Chatbot.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[43]:
+
+
+import io
+import random
+import string
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[44]:
+
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+nltk.download('popular', quiet=True)
+
+
+# In[45]:
+
+
+f=open("C:/Users/praku/OneDrive/Desktop/chatbot.txt",'r',errors = 'ignore')
+raw=f.read()
+raw = raw.lower()
+
+
+# In[46]:
+
+
+sent_tokens = nltk.sent_tokenize(raw) 
+word_tokens = nltk.word_tokenize(raw)
+
+
+# In[47]:
+
+
+lemmer = nltk.stem.WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+# In[48]:
+
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+def greeting(sentence):
+ 
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+# In[49]:
+
+
+def response(user_response):
+    Foxy_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        Foxy_response=Foxy_response+"I am sorry! I don't understand you"
+        return Foxy_response
+    else:
+        Foxy_response = Foxy_response+sent_tokens[idx]
+        return Foxy_response
+
+
+# In[50]:
+
+
+flag=True
+print("Foxy: My name is Foxy. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            print("Foxy: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                print("Foxy: "+greeting(user_response))
+            else:
+                print("Foxy: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("Foxy: Bye! take care..")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
